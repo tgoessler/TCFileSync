5b5da3ee7e8569ef86d3013da3f10ef28eed22f0
Thomas Gößler <the____tiger@hotmail.com>
2026-02-12 21:14:58 +0100

    updated to latest

--- create_win64.bat
+++ create_win64.bat
@@ -1,6 +1,6 @@
 set INSTALL_DIR=%~dp0\..\installed
 mkdir build_win64
 pushd build_win64
-cmake -G "Visual Studio 16 2019" -A "x64" -DBUILD_SHARED_LIBS=true -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..
+cmake -G "Visual Studio 17 2022" -A "x64" -DBUILD_SHARED_LIBS=true -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% ..
 popd
 pause
